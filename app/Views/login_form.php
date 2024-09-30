e6f8caa033cb519380badb2e670b4f2170400d0e
Ilham Maulana <ilhammmmmaulana@gmail.com>
2024-09-30 15:38:32 +0700

    m

--- app/Views/login_form.php
+++ app/Views/login_form.php
@@ -3,33 +3,17 @@
 
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sistem Asesmen OBE PS TIN</title>
-  <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Log in</title>
+  <link rel="icon" href="<?= base_url('assets/icons/internet.ico'); ?>" type="image/x-icon">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?= base_url('Adminlte/plugins/fontawesome-free/css/all.min.css'); ?>">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- overlayScrollbars -->
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?= base_url('Adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
+  <!-- Theme style -->
   <link rel="stylesheet" href="<?= base_url('Adminlte/dist/css/adminlte.min.css'); ?>">
-  
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-
-  <!-- daterange picker -->
-  <link rel="stylesheet" href="<?= base_url('plugins/daterangepicker/daterangepicker.css'); ?>">
-  <!-- iCheck for checkboxes and radio inputs -->
-  <link rel="stylesheet" href="<?= base_url('plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
-  <!-- Bootstrap Color Picker -->
-  <link rel="stylesheet" href="<?= base_url('plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css'); ?>">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="<?= base_url('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'); ?>">
-  <!-- Select2 -->
-  <link rel="stylesheet" href="<?= base_url('plugins/select2/css/select2.min.css'); ?>">
-  <link rel="stylesheet" href="<?= base_url('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css'); ?>">
-  <!-- Bootstrap4 Duallistbox -->
-  <link rel="stylesheet" href="<?= base_url('plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css'); ?>">
   <!-- Theme style -->
 
   <!-- Google Font: Source Sans Pro -->
